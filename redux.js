a284dd6912229d93df6acc215320427900b757bb
saramazal <32996161+saramazal@users.noreply.github.com>
2021-08-02 13:11:57 +0300

    Create redux.js
    The core concepts of Redux. 
    
    In this project  was building an adventure game using reducers, state, and actions. 
    The state will represent the state of the game.
    It contains the player’s inventory, distance travelled, and time on the road. 
    Each event in the game is represented as an action. 
    Players can gather supplies, travel, and–if they play risky–sometimes tip over the wagon carrying their supplies.

---
+++ redux.js
@@ -0,0 +1,83 @@
+const initialWagonState = {
+  supplies: 100,
+  distance: 0,
+  days: 0,
+  cash: 200
+};
+const roadReducer = (state = initialWagonState, action) => {
+    switch (action.type) {
+        case 'gather': {
+            return {
+      ...state,
+      supplies: state.supplies + 15,
+      days: state.days + 1
+    };
+  }
+  case 'travel': {
+            if (state.supplies - (20 * action.payload) < 0) {
+                return state;
+            } else {
+                return {
+                    ...state,
+                    supplies: state.supplies - (20 * action.payload),
+                    distance: state.distance + (10 * action.payload),
+                    days: state.days + action.payload
+                };
+            }
+       }
+        case 'tippedWagon': {
+          return {
+            ...state,
+            supplies: state.supplies - 30,
+            distance: state.distance,
+            days: state.days +1
+          };
+        }
+        case 'sell': {
+          return {
+            ...state,
+             supplies: state.supplies - 20,
+             distance: state.distance,
+             days: state.days,
+             cash: state.cash + 15
+            };
+        }
+        case 'buy': {
+          return {
+             ...state,
+             supplies: state.supplies + 25,
+             distance: state.distance,
+             days: state.days,
+             cash: state.cash - 10
+          };
+        }
+         case 'theft': {
+          return {
+             ...state,
+             supplies: state.supplies + 25,
+             distance: state.distance,
+             days: state.days,
+             cash: state.cash / 2
+          }
+        }
+    return state;
+  }
+};
+
+let wagon = roadReducer(undefined, {});
+console.log(wagon);
+
+wagon = roadReducer(wagon, { type: 'travel', payload: 1 });
+console.log(wagon);
+wagon = roadReducer(wagon, { type: 'gather' });
+console.log(wagon);
+wagon = roadReducer(wagon, { type: 'tippedWagon' });
+console.log(wagon);
+wagon = roadReducer(wagon, { type: 'travel', payload: 3 });
+console.log(wagon);
+wagon = roadReducer(wagon, { type: 'sell' });
+console.log(wagon);
+wagon = roadReducer(wagon, { type: 'buy' });
+console.log(wagon);
+wagon = roadReducer(wagon, { type: 'theft' });
+console.log(wagon);
